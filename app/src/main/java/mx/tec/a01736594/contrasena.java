f2d46213b385b261d0725a87814ff4a47066bd7a
Daniela Lozada <a01736594@tec.mx>
2023-10-08 23:23:07 -0600

    Initial commit

---
+++ app/src/main/java/mx/tec/a01736594/contrasena.java
@@ -0,0 +1,15 @@
+package mx.tec.a01736594;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class contrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contrasena);
+    }
+}
---
+++ app/src/main/java/mx/tec/a01736594/correoelectronico.java
@@ -0,0 +1,14 @@
+package mx.tec.a01736594;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class correoelectronico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_correoelectronico);
+    }
+}
