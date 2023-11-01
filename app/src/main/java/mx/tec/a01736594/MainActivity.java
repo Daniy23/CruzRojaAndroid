853284417de237c47380c2a27510dd69b0e50357
RogerHdzC <hdc.rogelio@gmail.com>
2023-10-31 19:01:55 -0600

    Splash screen Screen de inicio Integracion Firebase

--- app/src/main/java/mx/tec/a01736594/MainActivity.java
+++ app/src/main/java/mx/tec/a01736594/MainActivity.java
@@ -2,13 +2,34 @@ package mx.tec.a01736594;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Administrador);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginBtn = findViewById(R.id.login);
+
+        FirebaseAnalytics analytics = FirebaseAnalytics.getInstance(this);
+        Bundle bundle = new Bundle();
+        bundle.putString("message", "Integración de Firebase completa");
+        analytics.logEvent("InitScreen", bundle);
     }
+
+    public void loginAction(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
 }
