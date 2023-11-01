386f5e8ce19548e297f74fe7c60ce444b93a1c85
Daniela Lozada <a01736594@tec.mx>
2023-10-31 22:34:32 -0600

    MENU interfaz

--- app/src/main/java/mx/tec/a01736594/menuprincipal.java
+++ app/src/main/java/mx/tec/a01736594/menuprincipal.java
@@ -2,7 +2,9 @@ package mx.tec.a01736594;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class menuprincipal extends AppCompatActivity {
 
@@ -11,4 +13,25 @@ public class menuprincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuprincipal);
     }
+    public void dashboard(View v){
+        Intent intent = new Intent(this, dashboard.class);
+        startActivity(intent);
+    }
+    public void ajustes(View v){
+        Intent intent = new Intent(this, ajustes.class);
+        startActivity(intent);
+    }
+    public void anuncio(View v){
+        Intent intent = new Intent(this, anuncios.class);
+        startActivity(intent);
+    }
+    public void horasservicio(View v){
+        Intent intent = new Intent(this, horasservicio.class);
+        startActivity(intent);
+    }
+    public void login(View v){
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+    }
+
 }
