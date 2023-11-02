6ed1158524bdd2aea4eafefcbb0bb99fa91b1d4e
hugo1808 <128104575+hugo1808@users.noreply.github.com>
2023-11-02 12:45:15 -0600

    ConfigUserComplete (#4)
    * horaservicio
    
    * boton back
    
    * Actualizado con master (#3)
    
    * MenuTry
    
    * ConstrainMenu
    
    * Fondo
    
    * Boton
    
    * MENU interfaz
    
    * Login interfaz
    
    * dashboard interfaz
    
    * Firebase Auth (#2)
    
    * App Icon
    
    * Splash screen
    Screen de inicio
    Integracion Firebase
    
    * Inicio de configuración login
    
    * Rename .java to .kt
    
    * LogIn, SignUp y LogOut implementado
    
    * LogIn, SignUp y LogOut implementado
    
    ---------
    
    Co-authored-by: RogerHdzC <Âhdc.rogelio@gmail.com>
    Co-authored-by: RogerHdzC <hdc.rogelio@gmail.com>
    
    ---------
    
    Co-authored-by: Daniela Lozada <a01736594@tec.mx>
    Co-authored-by: RogerHdzC <Âhdc.rogelio@gmail.com>
    Co-authored-by: RogerHdzC <hdc.rogelio@gmail.com>
    
    * configusercompl
    
    * FinalUserConfig
    
    ---------
    
    Co-authored-by: Rogelio Hernández Cortés <92351154+RogerHdzC@users.noreply.github.com>
    Co-authored-by: Daniela Lozada <a01736594@tec.mx>
    Co-authored-by: RogerHdzC <Âhdc.rogelio@gmail.com>
    Co-authored-by: RogerHdzC <hdc.rogelio@gmail.com>

--- app/src/main/java/mx/tec/a01736594/horasservicio.java
+++ app/src/main/java/mx/tec/a01736594/horasservicio.java
@@ -2,7 +2,9 @@ package mx.tec.a01736594;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class horasservicio extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class horasservicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horasservicio);
+    }
+    public void go(View v){
+        Intent intent = new Intent(this, menuprincipal.class);
+        startActivity(intent);
+    }
+    public void logingo(View v){
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+    }
+    public void signupgo(View v){
+        Intent intent = new Intent(this, signup.class);
+        startActivity(intent);
     }
 }
