bd912acd164ac8c60e65f7a43c459a78d25e4cf7
Daniela Lozada <a01736594@tec.mx>
2023-11-02 13:01:21 -0600

    Revert "ConfigUserComplete (#4)"
    This reverts commit edb4305e5450f7b1d8bd4d129feaa26ee580df6d.

--- app/src/main/java/mx/tec/a01736594/signup.java
+++ app/src/main/java/mx/tec/a01736594/signup.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class horasservicio extends AppCompatActivity {
+public class signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_horasservicio);
+        setContentView(R.layout.activity_signup);
     }
 }
