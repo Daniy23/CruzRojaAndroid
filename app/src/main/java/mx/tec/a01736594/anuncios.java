f064364ebb21c36584ea6b3f91cffc4d8d26efbd
Daniela Lozada <a01736594@tec.mx>
2023-11-02 12:20:39 -0600

    BotonesReturn

--- app/src/main/java/mx/tec/a01736594/anuncios.java
+++ app/src/main/java/mx/tec/a01736594/anuncios.java
@@ -2,7 +2,9 @@ package mx.tec.a01736594;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class anuncios extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class anuncios extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anuncios);
+    }
+    public void go(View v){
+        Intent intent = new Intent(this, menuprincipal.class);
+        startActivity(intent);
     }
 }
