c4d8a79cbc194be8dbbcd92d008f8f774a3c912b
Daniela Lozada <a01736594@tec.mx>
2023-11-06 14:14:20 -0600

    modificacionecurso

--- app/src/main/java/mx/tec/a01736594/contrasena.java
+++ app/src/main/java/mx/tec/a01736594/contrasena.java
@@ -2,7 +2,9 @@ package mx.tec.a01736594;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class contrasena extends AppCompatActivity {
@@ -11,5 +13,9 @@ public class contrasena extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contrasena);
+    }
+    public void regresarajustes(View v){
+        Intent intent = new Intent(this, ajustes.class);
+        startActivity(intent);
     }
 }
